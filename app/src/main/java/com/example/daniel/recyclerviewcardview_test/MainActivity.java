5ccb8563445b9eabb25dc3cd2516cf1cab530861
DanielOlvera <josue_olver@yahoo.com.mx>
2016-12-04 16:41:08 -0500

    First Commit, will contain a single branch for each part

---
+++ app/src/main/java/com/example/daniel/recyclerviewcardview_test/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.daniel.recyclerviewcardview_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void doRecyclerVw(View view) {
+    }
+
+    public void doFragRecyclerVw(View view) {
+    }
+
+    public void doFragRecyclerVwAdd(View view) {
+    }
+}
---
+++ app/src/main/java/com/example/daniel/recyclerviewcardview_test/simple/SimpleRecycler.java
@@ -0,0 +1,15 @@
+package com.example.daniel.recyclerviewcardview_test.simple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.daniel.recyclerviewcardview_test.R;
+
+public class SimpleRecycler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_recycler);
+    }
+}
